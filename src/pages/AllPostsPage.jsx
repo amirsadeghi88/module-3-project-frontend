a1a23f3f8b2d86a9a0a670be57f6bd81572a58e6
amirsadeghi <amirsadeghi5991@gmail.com>
2025-06-30 09:42:28 +0200

    conveyed

--- src/pages/AllPostsPage.jsx
+++ src/pages/AllPostsPage.jsx
@@ -1,33 +1,7 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import React from "react";
 
-const AllPostsPage = () => {
-  const [posts, setPosts] = useState([]);
+function AllPostsPage() {
+  return <div></div>;
+}
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:5005/posts/all-posts")
-      .then((res) => {
-        console.log(res.data);
-        setPosts(res.data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
-  return (
-    <div>
-      <Navbar />
-      <h1>All Posts</h1>
-      {posts.map((post, index) => (
-        <div key={index}>
-          <h2>Hi my name is: {post.name}</h2>
-          <p>I need care on these dates: {post.dates}</p>
-          <Link to="/profile">Go back to my profile</Link>
-        </div>
-      ))}
-    </div>
-  );
-};
 export default AllPostsPage;
